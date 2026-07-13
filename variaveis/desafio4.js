82fed35909ea7debac6e879c7ef9a9fa0ab25c57
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-13 04:25:53 -0400

    feat: idade

---
+++ variaveis/desafio4.js
@@ -0,0 +1,9 @@
+/* 
+Crie uma variável com o ano atual e outra com o ano de nascimento.
+*/
+
+const anoAtual = 2026;
+const anoNascimento = 2005;
+
+const idade = anoAtual - anoNascimento;
+console.log('Sua idade é:', idade);
