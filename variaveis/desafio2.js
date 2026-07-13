0443e22cb4f601c9d134afc6da2435d400534d3a
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-13 04:16:00 -0400

    feat: realizei um exercicio de soma

---
+++ variaveis/desafio2.js
@@ -0,0 +1,7 @@
+// Crie duas variáveis e armazene dois números e exibir o resultado da soma
+
+const num1 = 15;
+const num2 = 8;
+
+const soma = 15 + 8;
+console.log('A soma dos dois números é:', soma);
