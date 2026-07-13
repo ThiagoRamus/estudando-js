32dbd04f640bf56730a3433eb8df9aee60bd9bf9
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-13 04:21:31 -0400

    feat: realizei um desafio basico de media

---
+++ variaveis/desafio3.js
@@ -0,0 +1,10 @@
+/* 
+Crie três notas e calcule a média.
+*/
+
+const nota1 = 8;
+const nota2 = 6;
+const nota3 = 9;
+
+const media = (nota1 + nota2 + nota3) / 3;
+console.log('Sua media é:', media.toFixed(2))
