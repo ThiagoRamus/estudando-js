6d6af59dc5fbe84ef747e347102e2189d53380b9
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-14 01:05:08 -0400

    feat: horas trabalhadas

---
+++ variaveis/desafio13.js
@@ -0,0 +1,9 @@
+/* 
+Exercício - Horas trabalhadas
+*/
+
+const valorPorHora = 28;
+const horasTrabalhadas = 176;
+
+const salarioBruto = valorPorHora * horasTrabalhadas;
+console.log('O seu salario bruto é de: R$', salarioBruto)
