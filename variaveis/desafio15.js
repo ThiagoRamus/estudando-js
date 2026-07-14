6dd7df2413788c6a2f8c1c5054d25d08d101d57d
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-14 01:37:43 -0400

    feat: diferença entre let e const

--- variaveis/desafio15.js
+++ variaveis/desafio15.js
@@ -6,7 +6,5 @@ idade = 24;
 const idade2 = 21;
 idade2 = 24;
 
-/* 
-let funciona por que é um tipo instavel, ela é algo irregular que altera com facilidade ou com o tempo, tipo um campeonato de futebol que a cada jogo o time que vence ganha 3 pontos, o que empata ganha 1 e o que perde nao ganha nada, o let se utiliza nesse aspecto
-const nao é possivel alterar por que é algo estavel que nao muda, como por exemplo uma bateria de celular que ao carregar 100% nao muda, ela fica constante, apesar de mudar ao descarregar, mas ao chegar ao 100, ela trava; é algo constante.
-*/
+/* let funciona por que é um tipo flexivel, ela é algo irregular que altera com facilidade ou com o tempo, tipo um campeonato de futebol que a cada jogo o time que vence ganha 3 pontos, o que empata ganha 1 e o que perde nao ganha nada, o let se utiliza nesse aspecto
+const nao é possivel alterar por que é algo estavel que nao muda, como por exemplo um cpf que uma vez definido, nunca mais altera. */
