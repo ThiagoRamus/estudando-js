9125f86d4e34cba48ad90c0ed6d29f6269a5ec1d
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-14 01:22:54 -0400

    feat: operadores

---
+++ variaveis/desafio14.js
@@ -0,0 +1,17 @@
+/* 
+Exercício - Operadores
+*/
+
+const a = 37;
+const b = 5;
+
+const soma = a + b;
+console.log('O valor de sua soma é:', soma)
+const subtracao = a - b;
+console.log('O valor de sua subtracao é:', subtracao)
+const multiplicacao = a * b;
+console.log('O valor de sua multiplicacao é:', multiplicacao)
+const divisao = a / b;
+console.log('O valor de sua divisao é:', divisao)
+const resto = a % b;
+console.log('O valor de resto é:', resto)
