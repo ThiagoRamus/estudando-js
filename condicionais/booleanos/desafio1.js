aef3b27106c6eab41b68a5dc68a180ff488713d3
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-15 01:38:30 -0400

    feat: carteira de motorista

---
+++ condicionais/booleanos/desafio1.js
@@ -0,0 +1,8 @@
+/* 
+Crie um programa que verifique se uma pessoa pode tirar a carteira de motorista.
+*/
+
+const idade = 18;
+
+const podeDirigir = idade >= 18;
+console.log('Pessoa pode tirar carteira : ', podeDirigir)
