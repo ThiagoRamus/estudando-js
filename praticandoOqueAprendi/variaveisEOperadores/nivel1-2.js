47d140ba16ed34d1dea897e02bd5bc6bafb7f8a9
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-17 03:53:32 -0400

    feat: eu to bagulho doido fi

---
+++ praticandoOqueAprendi/variaveisEOperadores/nivel1-2.js
@@ -0,0 +1,13 @@
+/* 
+1.2 Dado um número numero, descubra o resto da divisão dele por 2 usando o operador % e diga se ele é par ou ímpar 
+(sem usar if ainda nessa parte, só o cálculo — o if vem no próximo nível).. */
+
+const numero = 2;
+
+let resto = (numero % 2) === 0;
+const numeroPar = resto;
+resto = true;
+
+
+console.log('Par', numeroPar)
+console.log('Impar', !numeroPar)
