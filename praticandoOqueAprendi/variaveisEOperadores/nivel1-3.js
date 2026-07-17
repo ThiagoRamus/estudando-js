84acb63706c6e890eb9def0c81a02e3a12eddc47
ThiagoRamus <Thiagonunes3@esbam.edu.br>
2026-07-17 04:03:44 -0400

    feat: muito facil

---
+++ praticandoOqueAprendi/variaveisEOperadores/nivel1-3.js
@@ -0,0 +1,10 @@
+/* 
+1.3 Troque os valores de duas variáveis a e b sem usar uma terceira variável 
+(só com + e -, ou só com * e /).
+*/
+
+let a = 2;
+let b = 4;
+
+console.log(a = a * b)
+console.log(b = b / b)
